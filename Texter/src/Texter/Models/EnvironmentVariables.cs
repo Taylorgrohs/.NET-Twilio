ab9014d78781f863440f19d1c474c15769a5f82a
Taylor <taylorgrohs@gmail.com>
2016-05-10 09:40:22 -0700

    twilio with mvc

---
+++ Texter/src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "AC511da8b64e69517082a8911962dfab8c";
+        public static string AuthToken = "b07f4c2a9aaeac2f4d1feee0643dd25b";
+    }
+}
